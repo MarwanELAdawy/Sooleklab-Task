20f83fed071776ac2bd984422c6efc6336ffb204
MarwanELAdawy <marwan.mahmoud@cis.asu.edu.eg>
2019-06-04 16:50:45 +0200

    the final edit

--- src/app/product-edit/product-edit.component.ts
+++ src/app/product-edit/product-edit.component.ts
@@ -10,16 +10,53 @@ import { FormControl, FormGroupDirective, FormBuilder, FormGroup, NgForm, Valida
 export class ProductEditComponent implements OnInit {
 
   productForm: FormGroup;
-  id:string='';
+  id:number=null;
   prod_name:string='';
   prod_categories:string='';
-  prod_img:File=null;
+  prod_img:string='';
   prod_price:number=null;
   updated_at:Date=null;
   isLoadingResults = false;
   constructor(private router: Router, private route: ActivatedRoute, private api: ApiService, private formBuilder: FormBuilder) { }
 
+  getProduct(id) {
+    this.api.getProduct(id).subscribe(data => {
+      this.id = data.id;
+      this.productForm.setValue({
+        prod_name: data.prod_name,
+        prod_categories: data.prod_categories,
+        prod_img: data.prod_categories,
+        prod_price: data.prod_price,
+        updated_at: data.updated_at
+      });
+    });
+  }
+
+  onFormSubmit(form:NgForm) {
+    this.isLoadingResults = true;
+    this.api.updateProduct(this.id, form)
+      .subscribe(res => {
+          let id = res['id'];
+          this.isLoadingResults = false;
+          this.router.navigate(['/product-details', id]);
+        }, (err) => {
+          console.log(err);
+          this.isLoadingResults = false;
+        }
+      );
+  }
+
+  productDetails() {
+    this.router.navigate(['/product-details', this.id]);
+  }
+
   ngOnInit() {
+    this.getProduct(this.route.snapshot.params['id']);
+    this.productForm = this.formBuilder.group({
+    'prod_name' : [null, Validators.required],
+    'prod_desc' : [null, Validators.required],
+    'prod_price' : [null, Validators.required]
+  });
   }
 
 }
